5c62db447a901c1ef759c718eb944297b6ac733d
florezjose <florezjoserodolfo@gmail.com>
2019-09-07 12:15:51 -0500

    Added average practice algorithm.

---
+++ promedio-practicas/README.md
@@ -0,0 +1,3 @@
+### Promedio prácticas
+
+El promedio de prácticas de un curso se calcula en base a cuatro prácticas calificadas de las cuales se elimina la nota menor y se promedian las tres notas mas altas. Diseñe un algoritmo que determine la nota eliminada y el promedio de prácticas de un estudiante.
---
+++ promedio-practicas/practicas.py
@@ -0,0 +1,21 @@
+def __main__():
+    n = []
+    p = 0
+    for i in range(1, 5):
+        n1 = float(input('Ingrese nota {i}: '.format(i=i)))
+        if i > 1:
+            if n[i - 2] >= n1:
+                n.append(n1)
+            else:
+                n.insert(0, n1)
+        else:
+            n.append(n1)
+
+    for i in range(0, len(n) - 1):
+        p += n[i]
+
+    print("""Promedio: {p}""".format(p=p / 3))
+
+
+if __name__ == '__main__':
+    __main__()
