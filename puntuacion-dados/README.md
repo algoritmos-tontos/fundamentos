0d21bff15c59a112c45c062e7b70a23ba04c3564
florezjose <florezjoserodolfo@gmail.com>
2019-09-07 10:24:04 -0500

    Added algorithm of score dice.

---
+++ puntuacion-dados/README.md
@@ -0,0 +1,8 @@
+### Puntuación dados
+
+Diseñe un algoritmo que califique el puntaje obtenido en el lanzamiento de tres dados en funcion a la cantidad seis obtenidos de acuerdo a lo siguiente:
+
+- Seis en los tres dados, excelente.
+- Seis en dos dados, muy bien.
+- Seis en un dado, regular.
+- Ningun de seis, pésimo.
---
+++ puntuacion-dados/dados.py
@@ -0,0 +1,17 @@
+def __main__():
+    d1 = int(input('Ingrese dado 1: '))
+    d2 = int(input('Ingrese dado 1: '))
+    d3 = int(input('Ingrese dado 1: '))
+
+    if d1 == 6 and d1 == d2 == d3:
+        print('Puntuación: Excelente')
+    elif d1 == 6 == d2 or d1 == 6 == d3 or d2 == 6 == d3:
+        print('Puntuación: Muy bien')
+    elif d1 == 6 or d2 == 6 or d3 == 6:
+        print('Puntuación: Regular')
+    else:
+        print('Puntuación: Pésima')
+
+
+if __name__ == '__main__':
+    __main__()
